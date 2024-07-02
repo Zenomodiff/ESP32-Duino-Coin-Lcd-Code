9d5146a2bc3e0e58d399531d517314eabc1f2575
Sreeram Aj <sreeramzeno@gmail.com>
2024-07-02 12:28:05 +0530

    Add files via upload

---
+++ I2C LCD CODE/ESP_Code_Mining/Dashboard.h
@@ -0,0 +1,10 @@
+#ifndef DASHBOARD_H
+#define DASHBOARD_H
+
+const char WEBSITE[] PROGMEM = R"=====(
+    <!DOCTYPE html>
+    <html>
+    <!--
+        Duino-Coin self-hosted dashboard
+        MIT licensed
+#endif
